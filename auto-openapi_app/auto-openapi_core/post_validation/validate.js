bda4d71af3e865265da35f0cab4203c53eaf3a3a
caohanyang <caohanyang007@gmail.com>
2018-03-26 09:51:45 +0200

    update validation

--- auto-openapi_app/auto-openapi_core/post_validation/validate.js
+++ auto-openapi_app/auto-openapi_core/post_validation/validate.js
@@ -5,9 +5,11 @@ var path = require('path');
 const args = process.argv;
 var isJSON = require('is-json');
 var each = require('async-each-series');
+var util = require('util');
+var exec = require('child_process').exec;
 
 var API_NAME, CompareSet_PATH;
-var APIKEYNAME, APIKEYVALUE;
+var APIKEYNAME, APIKEYVALUE, APIKEYNAMEPASS, APIKEYVALUEPASS;
 
 readConsole();
 
@@ -17,7 +19,13 @@ readAuth();
 
 
 const openAPI = require(CompareSet_PATH + "/OpenAPI.json");
-var baseUrl = openAPI.schemes[0] + "://" + openAPI.host + openAPI.basePath;
+var baseUrl 
+if (openAPI.basePath) {
+	baseUrl = openAPI.schemes[0] + "://" + openAPI.host + openAPI.basePath;
+} else {
+	baseUrl = openAPI.schemes[0] + "://" + openAPI.host;
+}
+
 // var baseUrl = openAPI.schemes[0]+ "://" + openAPI.host + openAPI.basePath;
 var pathObj = openAPI.paths;
 console.log(pathObj);
@@ -43,8 +51,10 @@ each(Object.keys(pathObj), function (urlKey, next) {
 				// Verb part
 				var paraObj, reqObj, resObj;
 				var composeUrl = oUrl;
-
+                var postbody;
 				console.log("----------Verb part------------");
+				console.log(verbKey);
+
 				if (verbObj[verbKey].hasOwnProperty('parameters')) {
 					paraObj = verbObj[verbKey]['parameters'];
 				};
@@ -54,65 +64,99 @@ each(Object.keys(pathObj), function (urlKey, next) {
 				if (verbObj[verbKey].hasOwnProperty('responses')) {
 					resObj = verbObj[verbKey]['responses'];
 				};
-
+				console.log("----------req part------------");
 				console.log(reqObj);
-				console.log(urlKey);
+				
 				if (reqObj !== undefined) {
 					if (reqObj.startsWith('http')) {
 						composeUrl = reqObj;
+						composeUrl = replaceAuth(composeUrl, oUrl);
 					} else if (reqObj.startsWith('/')) {
 						if (oUrl.includes("?")) {
 							composeUrl = oUrl.split("?")[0] + '?' + reqObj.split("?")[1]
+							composeUrl = replaceAuth(composeUrl, oUrl);
+						}
+					} else if (reqObj.startsWith('{') | reqObj.startsWith('[')) {
+						composeUrl = replaceAuth(composeUrl, oUrl);
+						postbody = reqObj;
+					} else if (reqObj.startsWith('curl')) {
+						composeUrl = reqObj;
+						composeUrl = replaceAuthCurl(composeUrl);
+					}
+				} else {
+					composeUrl = replaceAuth(composeUrl, oUrl);
+				}
+				console.log("-------composeUrl-----------");
+				console.log(composeUrl);
+
+				if (!composeUrl.startsWith('curl')) {
+					var options;
+					if (verbKey.includes("POST") && postbody!==null) {
+						options = { url: composeUrl, method: verbKey, data: postbody, headers: {"Content-Type": "application/json"}, include: true };
+					} else {
+						options = { url: composeUrl, method: verbKey, include: true };
+					}
+				
+					curl.request(options, function (err, parts) {
+						var full = parts;
+						var statusCode;
+						console.log("---------full-----------");
+						console.log(full);
+						if (typeof full == 'undefined' || !full) {
+							
+							statusCode = "404";
+						} else {
+							parts = parts.split('\r\n');
+							var data = parts.pop()
+								, head = parts.pop();
+	
+							console.log("---------data-----------");
+							console.log(data);
+							console.log("---------head-----------");
+							if (head.length === 0) {
+								head = parts;
+							}
+							console.log(head);
+	
+							statusCode = getStatusCode(head);
+	
 						}
 
-					}
-				}
+						var realResponse = {};
+						realResponse[statusCode] = data;
 
-				console.log(composeUrl);
-				if (composeUrl.includes(APIKEYNAME)) {
-
-					var realvalue = composeUrl.substring(composeUrl.indexOf(APIKEYNAME))
-					if (realvalue.includes('&')) {
-						realvalue = realvalue.split('&')[0].split('=')[1]
-					} else {
-						realvalue = realvalue.split('=')[1]
-					}
-
-					composeUrl = composeUrl.replace(realvalue, APIKEYVALUE);
+						openAPI.paths[urlKey][verbKey]["realresponses"] = realResponse;
+						next();
+						
+					});
 				} else {
-					composeUrl = oUrl + "?" + APIKEYNAME + "=" + APIKEYVALUE;
+					child = exec(composeUrl, function (error, stdout, stderr) {
+						var data;
+						console.log('stdout: ' + stdout);
+
+						if (stdout.includes('error')) {
+							statusCode = "404";
+						} else {
+							statusCode = "200";
+						}
+						data = stdout
+
+						if (error !== null) {
+							console.log('exec error: ' + error);
+							statusCode = "404";
+							data = error.message;
+						}
+
+						var realResponse = {};
+						realResponse[statusCode] = data;
+
+						openAPI.paths[urlKey][verbKey]["realresponses"] = realResponse;
+						next();
+
+					});
+
+
 				}
-
-				console.log(composeUrl);
-
-
-				var options = { url: composeUrl, method: verbKey, include: true };
-
-
-				curl.request(options, function (err, parts) {
-					var full = parts;
-
-					parts = parts.split('\r\n');
-					var data = parts.pop()
-						, head = parts.pop();
-
-					console.log("---------data-----------");
-					// console.log(data);
-					console.log("---------head-----------");
-					if (head.length === 0) {
-						head = parts;
-					}
-					console.log(head);
-
-					var statusCode = getStatusCode(head);
-
-					var realResponse = {};
-					realResponse[statusCode] = data;
-
-					openAPI.paths[urlKey][verbKey]["realresponses"] = realResponse;
-					next();
-				});
-
 			}, 1000);
 		}, function (err) {
 			console.log("finished one loop")
@@ -127,12 +171,6 @@ each(Object.keys(pathObj), function (urlKey, next) {
 })
 
 
-
-console.log("---------startwrite-------");
-
-
-
-
 function getStatusCode(parts) {
 	var statusCode = parts[0].split(' ')[1];
 	console.log(statusCode);
@@ -140,6 +178,66 @@ function getStatusCode(parts) {
 }
 
 
+function replaceAuthCurl(composeUrl) {
+	if (APIKEYNAME===undefined) return composeUrl;
+
+	if (composeUrl.includes(APIKEYNAME)) {
+
+		if (APIKEYNAME.includes("username")) {
+			composeUrl = composeUrl.replace(APIKEYNAME, APIKEYVALUE);
+			composeUrl = composeUrl.replace(APIKEYNAMEPASS, APIKEYVALUEPASS);
+		} else {
+			composeUrl = composeUrl.replace(APIKEYNAME, APIKEYVALUE);
+		}
+		
+	}
+
+	return composeUrl;
+}
+
+function replaceAuth(composeUrl, oUrl) {
+
+	if (APIKEYNAME===undefined) return composeUrl;
+   
+	if (APIKEYNAME.includes("|")) {
+		var arrAPIKEYNAME =  APIKEYNAME.split("|");
+		for (let i =0;i <arrAPIKEYNAME.length; i++) {
+			if (composeUrl.includes(arrAPIKEYNAME[i])) {
+				var realvalue = composeUrl.substring(composeUrl.indexOf(arrAPIKEYNAME[i]))
+				if (realvalue.includes('&')) {
+					realvalue = realvalue.split('&')[0].split('=')[1]
+				} else {
+					realvalue = realvalue.split('=')[1]
+				}
+		
+				composeUrl = composeUrl.replace(realvalue, APIKEYVALUE);
+				return composeUrl;
+			}
+		}
+	} else {
+		if (composeUrl.includes(APIKEYNAME)) {
+			
+					var realvalue = composeUrl.substring(composeUrl.indexOf(APIKEYNAME))
+					if (realvalue.includes('&')) {
+						realvalue = realvalue.split('&')[0].split('=')[1]
+					} else {
+						realvalue = realvalue.split('=')[1]
+					}
+			
+					composeUrl = composeUrl.replace(realvalue, APIKEYVALUE);
+		} else {
+			if (composeUrl.includes('&')) {
+				composeUrl = composeUrl + "&"+ APIKEYNAME + '=' + APIKEYVALUE;
+			} else {
+				composeUrl = composeUrl + "?"+ APIKEYNAME + '=' + APIKEYVALUE;
+			}
+			
+		}
+	}
+	
+
+	return composeUrl;
+}
 
 function readConsole() {
 	if (args.length > 0) {
@@ -164,5 +262,13 @@ function readAuth() {
 	if (configuration.indexOf("APIKEYVALUE") != -1) {
 		APIKEYVALUE = configuration.substring(configuration.indexOf("APIKEYVALUE")).split('\n')[0].split('=')[1];
 		console.log(APIKEYVALUE);
+	}
+	if (configuration.indexOf("APIKEYNAMEPASS") != -1) {
+		APIKEYNAMEPASS = configuration.substring(configuration.indexOf("APIKEYNAMEPASS")).split('\n')[0].split('=')[1];
+		console.log(APIKEYNAMEPASS);
+	}
+	if (configuration.indexOf("APIKEYVALUEPASS") != -1) {
+		APIKEYVALUEPASS = configuration.substring(configuration.indexOf("APIKEYVALUEPASS")).split('\n')[0].split('=')[1];
+		console.log(APIKEYVALUEPASS);
 	}
 }
